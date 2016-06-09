0567dfd43f6c17c1fe818c167d7ad62b84aa80a0
zodiac612 <sb240@gmx.de>
2016-06-09 13:12:10 +0200

    update

--- webradio.py
+++ webradio.py
@@ -5,17 +5,17 @@ import ConfigParser
 from time import sleep
 
 def threadWebradioService(sPathToConfig = '/var/sensorTool/www/webradio.station'):
-    config = ConfigParser.RawConfigParser()
-    config.read(sPathToConfig)
+    configRadio = ConfigParser.RawConfigParser()
+    configRadio.read(sPathToConfig)
     
-    if config.getboolean('running', 'changed'):
-        sArg = config.get('running',  'action') + ' ' + config.get('running',  'stream')+' '+config.get('running',  'volume')
+    if configRadio.getboolean('running', 'changed'):
+        sArg = configRadio.get('running',  'action') + ' ' + configRadio.get('running',  'stream')+' '+configRadio.get('running',  'volume')
         #print sArg
         os.system("/home/pi/webradio/webradio.sh " + sArg +" &")
         #os.system("echo \""+sArg+"\"")
-        config.set('running', 'changed', False)
-        with open(str(sPathToConfig), 'wb') as configfile:
-            config.write(configfile) 
+        configRadio.set('running', 'changed', False)
+        with open(str(sPathToConfig), 'wb') as configRadioFile:
+            configRadio.write(configRadioFile) 
    
 def threadwebradiotest():
     while True:
